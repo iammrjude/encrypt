0ff9a6054152f00ce35a712b8e46711e84e23012
Marc Cuenca <mcuenca96@gmail.com>
2018-03-20 16:41:57 +0100

    Changes

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,24 @@
 import sys
 
 
-def encrypt(file_object):
+def encrypt(file_object, keyValue):
     for line in file_object:
         for ch in line:
-            if ch != ' ':
-                print ch
+            if ch != ' ' or ch != '.' or ch != ',' or ch != '"':
+                print encode(ch, keyValue),
 
+
+def encode(ch, keyValue):
+    newCh = (ord(ch)+keyValue)
+    if newCh == 'j' or newCh == 'w' or newCh == 'k':
+        newCh == (ord(ch)+keyValue)
+    if newCh > ord('z'):
+        newCh -= 26
+
+    return chr(newCh)
 
 
 if __name__ == '__main__':
     file_object = open("catil-1.1.txt", "r")
-    encrypt(file_object)
+    keyValue = int(sys.argv[1])
+    encrypt(file_object, keyValue)
